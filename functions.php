fd8ebbc7aa7cfdd6e7ed3702a9e18053ca5e5536
Isaac Castillo <icemancast@gmail.com>
2019-11-11 08:45:26 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -304,3 +304,30 @@ add_filter( 'widget_tag_cloud_args', 'echo_widget_tag_cloud_args' );
  */
 require get_template_directory() . '/includes/BootstrapWalker.php';
 
+/**
+ * Add options to our themes
+ */
+if( function_exists('acf_add_options_page') ) {
+
+	acf_add_options_page(array(
+		'page_title' 	=> 'Theme General Settings',
+		'menu_title'	=> 'Theme Settings',
+		'menu_slug' 	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Theme Header Settings',
+		'menu_title'	=> 'Header',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Theme Footer Settings',
+		'menu_title'	=> 'Footer',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+
+}
+
