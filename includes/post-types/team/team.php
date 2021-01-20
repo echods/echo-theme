cb5af200bcf91e2e22658ea3b18b0ca2b9402413
Isaac Castillo <isaac@echods.com>
2021-01-20 13:38:01 -0600

    fixing example post type

--- includes/post-types/team/team.php
+++ includes/post-types/team/team.php
@@ -1,22 +1,5 @@
 <?php
-/*
- * Plugin Name: Echo Team
- * Description: Add team members
- * Version: 1.0.0
- * Author: Isaac Castillo
- * License: GPL2
-/*  Copyright 2016 Isaac Castillo  (email : isaac@echods.com)
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as
-    published by the Free Software Foundation.
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+
 if (!defined('ECHO_TEAM_DIR')) {
     define('ECHO_TEAM_DIR', dirname(__FILE__));
 }
